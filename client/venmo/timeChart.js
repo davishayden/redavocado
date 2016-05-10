4239088b5360e3d8fe4ec102be71a31ea18e5f6a
davishayden <1hayden.davis@gmail.com>
2016-05-09 23:57:14 -0400

    stuff

--- client/venmo/timeChart.js
+++ client/venmo/timeChart.js
@@ -10,7 +10,11 @@ function buildContainer2() {
             text: '1,000,000 transactions'
         },
         xAxis: {
-            categories: ['12AM','1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM','8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM','11PM']
+          /*  categories: ['12AM','1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM','8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM','11PM']*/
+          categories: ['8PM/5PM','9PM/6PM', '10PM/7PM', '11PM/8PM', '12AM/9PM', '1AM/10PM', '2AM/11PM', '3AM/12AM','4AM/1AM', '5AM/2AM', '6AM/3AM', '7AM/4AM', '8AM/5AM', '9AM/6AM', '10AM/7AM', '11AM/8AM', '12PM/9AM', '1PM/10AM', '2PM/11AM', '3PM/12PM', '4PM/1PM', '5PM/2PM', '6PM/3PM','7PM/4PM'],
+          title: {
+              text: 'ET/PT'
+          }
         },
         yAxis: {
             title: {
